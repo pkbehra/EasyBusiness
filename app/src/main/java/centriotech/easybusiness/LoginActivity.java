e2bfe49417f17fe589722c74d7e29dda9f6d90f5
Technokarts@123 <priyanka.nevase@technokarts.com>
2022-02-24 21:19:57 +0530

    Pritesh Commit

--- app/src/main/java/centriotech/easybusiness/LoginActivity.java
+++ app/src/main/java/centriotech/easybusiness/LoginActivity.java
@@ -2,13 +2,46 @@ package centriotech.easybusiness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView forgotpass;
+    EditText et_num, et_pass;
+    Button btn_signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_num = findViewById(R.id.phone);
+        et_pass = findViewById(R.id.password);
+        btn_signin = findViewById(R.id.btn_login);
+        forgotpass = findViewById(R.id.tvForgotPass);
+
+        forgotpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ForgotPassActivity.class);
+                startActivity(intent);
+//                finish();
+            }
+        });
+
+        btn_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(intent);
+//                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/centriotech/easybusiness/WelcomeActivity.java
+++ app/src/main/java/centriotech/easybusiness/WelcomeActivity.java
@@ -1,14 +1,79 @@
 package centriotech.easybusiness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import centriotech.easybusiness.fragment.Admindashboard;
+import centriotech.easybusiness.fragment.Adminprofile;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    BottomNavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        nav_view = findViewById(R.id.nav_view);
+
+        Admindashboard admindashboard = new Admindashboard();
+        FragmentManager fmmm = getSupportFragmentManager();
+        fmmm.beginTransaction().replace(R.id.frame, admindashboard).commit();
+
+
+        nav_view.setOnNavigationItemSelectedListener(navListener);
+
+        nav_view.setItemBackground(null);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    Admindashboard admindashboard = new Admindashboard();
+                    FragmentManager fmmm = getSupportFragmentManager();
+                    fmmm.beginTransaction().replace(R.id.frame, admindashboard).addToBackStack("Admin").commit();
+
+
+                    break;
+
+
+                case R.id.navigation_dashboard:
+
+
+//                    Dashboard dashboard = new Dashboard();
+//                    FragmentManager fmmms = getSupportFragmentManager();
+//                    fmmms.beginTransaction().replace(R.id.frame, dashboard).addToBackStack("Teacher").commit();
+
+
+                    Toast.makeText(getApplicationContext(), "Dashboard", Toast.LENGTH_SHORT).show();
+
+
+                    break;
+                case R.id.navigation_profile:
+
+                    Adminprofile adminprofile = new Adminprofile();
+                    FragmentManager fmmm1 = getSupportFragmentManager();
+                    fmmm1.beginTransaction().replace(R.id.frame, adminprofile).commit();
+
+
+                    break;
+
+
+            }
+            return true;
+        }
+    };
+
+
 }
