540ff45c61b2c09e37f04deca6bb26aa0575475d
Technokarts@123 <priyanka.nevase@technokarts.com>
2022-02-24 17:54:55 +0530

    Pritesh commit

---
+++ app/src/main/java/centriotech/easybusiness/VerifyenterOtp.java
@@ -0,0 +1,13 @@
+package centriotech.easybusiness;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyenterOtp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifyenter_otp);
+    }
+}
