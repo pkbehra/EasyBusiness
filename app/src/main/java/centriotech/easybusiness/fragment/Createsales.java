b8e5d2c2088d0ce279d9c5f683917031e87befa0
Technokarts@123 <priyanka.nevase@technokarts.com>
2022-02-25 20:59:29 +0530

    Pritesh Commit

--- app/src/main/java/centriotech/easybusiness/fragment/Createsales.java
+++ app/src/main/java/centriotech/easybusiness/fragment/Createsales.java
@@ -1,4 +1,22 @@
 package centriotech.easybusiness.fragment;
 
-public class Createsales {
-}
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import centriotech.easybusiness.R;
+
+public class Createsales extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.createsale, container, false);
+        return view;
+    }
+    }
--- app/src/main/java/centriotech/easybusiness/fragment/Viewsales.java
+++ app/src/main/java/centriotech/easybusiness/fragment/Viewsales.java
@@ -4,18 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import centriotech.easybusiness.R;
 
 public class Viewsales extends Fragment {
+
+    CardView view_add_sale, view_manage_sales;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.viewsales,container,false);
+        view_add_sale = view.findViewById(R.id.view_add_sale);
+        view_manage_sales = view.findViewById(R.id.view_manage_sales);
+
+        view_manage_sales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Manage Sales", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        view_add_sale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FragmentManager fm = getFragmentManager();
+                Createsales createsales = new Createsales();
+                fm.beginTransaction().replace(R.id.frame, createsales).addToBackStack("addconsignment").commit();
+
+
+            }
+        });
+
         return view;
     }
 }
